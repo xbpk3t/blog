971df9939af3f38fc7cb545ae200683e11a50928
XBPk3T <yyzw@live.com>
2024-10-07 00:47:11 +0800

    feat: reading-methods.mdx

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  projectName: 'blog',
+  projectName: 'xbpk3t.github.io',
   organizationName: 'xbpk3t',
   trailingSlash: false,
   deploymentBranch: 'gh-pages',
